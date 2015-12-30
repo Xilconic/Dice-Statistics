babef372304b0bd2481f118d6b3308d00d7ae88b
Xilconic <Xilconic@users.noreply.github.com>
2015-12-30 22:59:57 +0100

    SixSidedDie with RNG
    Extended SixSidedDie to use a RandomIntegerGenerator.

--- dice/src/main/java/com/xilconic/gaming/dice/SixSidedDie.java
+++ dice/src/main/java/com/xilconic/gaming/dice/SixSidedDie.java
@@ -1,10 +1,24 @@
 package com.xilconic.gaming.dice;
 
+import com.xilconic.gaming.dice.random.RandomIntegerGenerator;
+
 /**
  * Class representing a 6-sided die.
  */
 public class SixSidedDie implements Die {
     private boolean isRolled;
+    private RandomIntegerGenerator rng;
+    private int rolledValue;
+
+    /**
+     * Instantiates a new instance of {@link SixSidedDie} using the given random number generator
+     * to determine rolled results.
+     *
+     * @param rng The random number generator.
+     */
+    public SixSidedDie(RandomIntegerGenerator rng) {
+        this.rng = rng;
+    }
 
     @Override
     public boolean isRolled() {
@@ -14,6 +28,7 @@ public class SixSidedDie implements Die {
     @Override
     public void roll() {
         isRolled = true;
+        rolledValue = rng.getFromInclusiveRange(1, 6);
     }
 
     @Override
@@ -21,6 +36,6 @@ public class SixSidedDie implements Die {
         if (!isRolled) {
             throw new IllegalStateException("Method 'roll()' should have been called before calling 'getRolledValue()'.");
         }
-        return 0;
+        return rolledValue;
     }
 }
--- dice/src/test/java/com/xilconic/gaming/dice/test/SixSidedDieTest.java
+++ dice/src/test/java/com/xilconic/gaming/dice/test/SixSidedDieTest.java
@@ -1,9 +1,13 @@
 package com.xilconic.gaming.dice.test;
 
 import com.xilconic.gaming.dice.SixSidedDie;
+import com.xilconic.gaming.dice.test.mocks.PredefinedRandomIntegerGenerator;
+import com.xilconic.gaming.dice.test.mocks.RandomIntegerGeneratorStub;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 /**
  * Unit tests for {@link SixSidedDie}.
@@ -12,14 +16,17 @@ public class SixSidedDieTest {
 
     @Test
     public void testDefaultConstructor() {
-        SixSidedDie die = new SixSidedDie();
+        SixSidedDie die = new SixSidedDie(new RandomIntegerGeneratorStub());
 
         Assert.assertEquals(false, die.isRolled());
     }
 
     @Test
     public void testRoll() {
-        SixSidedDie die = new SixSidedDie();
+        ArrayList<Integer> results = new ArrayList<>();
+        results.add(1);
+
+        SixSidedDie die = new SixSidedDie(new PredefinedRandomIntegerGenerator(results));
 
         die.roll();
 
@@ -27,8 +34,27 @@ public class SixSidedDieTest {
     }
 
     @Test
+    public void testGetRolledResultAfterRoll() {
+        ArrayList<Integer> results = new ArrayList<>();
+        for (int i = 1; i <= 6; i++) {
+            results.add(i);
+        }
+
+        SixSidedDie die = new SixSidedDie(new PredefinedRandomIntegerGenerator(results));
+
+        for (int i = 0; i < results.size(); i++) {
+            die.roll();
+
+            Assert.assertEquals((int) results.get(i), die.getRolledValue());
+        }
+    }
+
+    @Test
     public void testGetRolledResultBeforeRolling() {
-        SixSidedDie die = new SixSidedDie();
+        ArrayList<Integer> results = new ArrayList<>();
+        results.add(1);
+
+        SixSidedDie die = new SixSidedDie(new PredefinedRandomIntegerGenerator(results));
 
         try {
             die.getRolledValue();
