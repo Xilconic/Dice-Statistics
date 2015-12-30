e2bdcec291f7598d6ef545e0dd6b1f54e0169c4d
Xilconic <Xilconic@users.noreply.github.com>
2015-12-30 16:41:24 +0100

    Created dice module
    Defined new Java module called "dice" (namespace
    com.xilconic.gaming.dice) and corresponding tests. Has initial class and
    interface implementations

---
+++ dice/src/main/java/com/xilconic/gaming/dice/SixSidedDie.java
@@ -0,0 +1,26 @@
+package com.xilconic.gaming.dice;
+
+/**
+ * Class representing a 6-sided die.
+ */
+public class SixSidedDie implements Die {
+    private boolean isRolled;
+
+    @Override
+    public boolean isRolled() {
+        return isRolled;
+    }
+
+    @Override
+    public void roll() {
+        isRolled = true;
+    }
+
+    @Override
+    public int getRolledValue() throws IllegalStateException {
+        if (!isRolled){
+            throw new IllegalStateException("Method 'roll()' should have been called before calling 'getRolledValue()'.");
+        }
+        return 0;
+    }
+}
---
+++ dice/src/test/java/com/xilconic/gaming/dice/test/SixSidedDieTest.java
@@ -0,0 +1,38 @@
+package com.xilconic.gaming.dice.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.xilconic.gaming.dice.SixSidedDie;
+
+public class SixSidedDieTest {
+
+    @Test
+    public void testDefaultConstructor(){
+        SixSidedDie die = new SixSidedDie();
+
+        Assert.assertEquals(false, die.isRolled());
+    }
+
+    @Test
+    public void testRoll(){
+        SixSidedDie die = new SixSidedDie();
+
+        die.roll();
+
+        Assert.assertEquals(true, die.isRolled());
+    }
+
+    @Test
+    public void testGetRolledResultBeforeRolling(){
+        SixSidedDie die = new SixSidedDie();
+
+        try{
+            die.getRolledValue();
+            Assert.fail("An 'IllegalStateException' should be thrown when 'getRolledValue()' is called before 'roll()'.");
+        }
+        catch (IllegalStateException e){
+            Assert.assertEquals("Method 'roll()' should have been called before calling 'getRolledValue()'.", e.getMessage());
+        }
+    }
+}
